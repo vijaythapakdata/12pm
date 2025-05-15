8a0efca87afc32faf10c3bcb355bfd969ad36ebf
vijaythapakdata <vijaythapak24@gmail.com>
2025-05-15 12:37:01 +0530

    newupdates

--- src/webparts/helloWorld/components/HelloWorld.tsx
+++ src/webparts/helloWorld/components/HelloWorld.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 // import styles from './HelloWorld.module.scss';
 import type { IHelloWorldProps } from './IHelloWorldProps';
 
-import { PrimaryButton, DefaultButton, IconButton } from '@fluentui/react';
+import { PrimaryButton, DefaultButton, IconButton , TextField, ChoiceGroup, Dropdown, ComboBox, Slider,  Checkbox, Toggle, SearchBox, Link, SpinButton, DatePicker} from '@fluentui/react';
 export default class HelloWorld extends React.Component<IHelloWorldProps> {
   public render(): React.ReactElement<IHelloWorldProps> {
 
@@ -15,6 +15,63 @@ export default class HelloWorld extends React.Component<IHelloWorldProps> {
     
     <DefaultButton text='Edit' iconProps={{iconName:'edit'}}/>&nbsp;&nbsp;&nbsp;&nbsp;
     <IconButton text='Delete' iconProps={{iconName:'delete'}}/>
+    <hr/>
+    <SearchBox placeholder='search here' iconProps={{iconName:'search'}}/>
+    <br/>
+    <br/>
+    <form>
+      {/* <Label> Full Name</Label> */}
+      <TextField type='text' label='Full Name' placeholder='vijay thapak' required/>
+      <TextField type='password' label='Password'/>
+      <TextField type='text' multiline rows={5} placeholder='Enter your address....' iconProps={{iconName:'home'}} label='Permanent Address'/>
+      <TextField prefix='$' label='Salary' suffix='$USD'/>
+      <TextField label='I am read only' disabled/>
+      <TextField label='Error Message' errorMessage='Please enter your password'/>
+      <ChoiceGroup
+      options={[
+        {key:'Male',text:'Male'},
+        {key:'Female',text:'Female'}
+      ]}
+      label='Gender'
+      />
+      <Dropdown
+      label='Department'
+      options={[
+        {key:'IT',text:'IT'},
+        {key:'HR',text:'HR'},
+        {key:'Account',text:'Account'}
+      ]}
+      placeholder='--select--'
+      />
+       <Dropdown
+      label='Multi Select'
+      options={[
+        {key:'IT',text:'IT'},
+        {key:'HR',text:'HR'},
+        {key:'Account',text:'Account'}
+      ]}
+      placeholder='--select--'
+      multiSelect
+      />
+       <ComboBox
+      label='Multi Select'
+      options={[
+        {key:'IT',text:'IT'},
+        {key:'HR',text:'HR'},
+        {key:'Account',text:'Account'}
+      ]}
+      placeholder='--select--'
+      multiSelect
+      allowFreeInput
+      autoComplete='on'
+      />
+      <Slider label='Score' min={0} max={100} step={1}/>
+      <Checkbox label='Consent'/>
+      <Toggle label='Permission' onText='ON' offText='OFF' defaultChecked/>
+      <Link href='https://www.google.com'>Google</Link>
+      <SpinButton min={0} max={100} step={1} />
+      <DatePicker label='DOJ'/>
+    </form>
     </>
     );
   }
